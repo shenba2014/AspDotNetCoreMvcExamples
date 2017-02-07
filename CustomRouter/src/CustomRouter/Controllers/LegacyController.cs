1669f5f6d9c20d5b82a8362fa8039c9993687e21
luojunwen <luojunwenbashen@163.com>
2017-02-07 22:59:51 +0800

    Add custom router

---
+++ CustomRouter/src/CustomRouter/Controllers/LegacyController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CustomRouter.Controllers
+{
+    public class LegacyController : Controller
+    {
+        public ViewResult GetLegacyUrl(string legacyUrl)
+                => View("GetLegacyUrl", legacyUrl);
+    }
+}
---
+++ CustomRouter/src/CustomRouter/LegacyRoute.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Internal;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CustomRouter
+{
+    public class LegacyRoute : IRouter
+    {
+        private readonly string[] _urls;
+        private readonly IRouter _mvcRoute;
+
+        public LegacyRoute(IServiceProvider services, params string[] urls)
+        {
+            _urls = urls;
+            _mvcRoute = services.GetRequiredService<MvcRouteHandler>();
+        }
+
+        public async Task RouteAsync(RouteContext context)
+        {
+            var requestedUrl = context.HttpContext.Request.Path.Value.TrimEnd('/');
+            if (_urls.Contains(requestedUrl, StringComparer.OrdinalIgnoreCase))
+            {
+                context.RouteData.Values["controller"] = "Legacy";
+                context.RouteData.Values["action"] = "GetLegacyUrl";
+                context.RouteData.Values["legacyUrl"] = requestedUrl;
+                await _mvcRoute.RouteAsync(context);
+            }
+        }
+
+        public VirtualPathData GetVirtualPath(VirtualPathContext context)
+        {
+            if (context.Values.ContainsKey("legacyUrl"))
+            {
+                var url = context.Values["legacyUrl"] as string;
+                if (_urls.Contains(url))
+                {
+                    return new VirtualPathData(this, url);
+                }
+            }
+            return null;
+        }
+    }
+}
