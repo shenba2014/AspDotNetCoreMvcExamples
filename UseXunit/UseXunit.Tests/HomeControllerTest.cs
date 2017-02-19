46b250b1244258daa91495c57a9825636722dab0
luojunwen <luojunwenbashen@163.com>
2017-02-19 23:47:02 +0800

    Add xUnit example

---
+++ UseXunit/UseXunit.Tests/HomeControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UseXunit.Controllers;
+using Xunit;
+
+namespace UseXunit.Tests
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void ShouldGetIndexResult()
+        {
+            var homeController = new HomeController();
+            var contentResult = homeController.Index() as ContentResult;
+            Assert.NotNull(contentResult);
+            Assert.Equal("Hello test", contentResult.Content);
+        }
+    }
+}
---
+++ UseXunit/src/UseXunit/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UseXunit.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello test");
+        }
+    }
+}
