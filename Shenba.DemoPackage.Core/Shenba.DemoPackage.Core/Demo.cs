7d9145124ad0043873560d0c41302814f3da685e
luojunwen <luojunwenbashen@163.com>
2017-03-12 22:44:49 +0800

    just a project for creating demo package which can be used in .NET Core application

---
+++ Shenba.DemoPackage.Core/Shenba.DemoPackage.Core/Demo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Shenba.DemoPackage.Core
+{
+	/// <summary>
+	/// Demo
+	/// </summary>
+	public class Demo
+	{
+		/// <summary>
+		/// Run
+		/// </summary>
+		public void Run()
+		{
+			Console.WriteLine($"You're using my demo package at {DateTime.Now.ToString(CultureInfo.InvariantCulture)}");
+		}
+	}
+}
