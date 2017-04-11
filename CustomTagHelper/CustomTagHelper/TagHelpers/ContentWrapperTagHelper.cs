8e004c3f23ac04e5dc53c2488aa9c89170c6cd1a
luojunwen <luojunwenbashen@163.com>
2017-04-11 23:40:37 +0800

    implement some advance feature

---
+++ CustomTagHelper/CustomTagHelper/TagHelpers/ContentWrapperTagHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace CustomTagHelper.TagHelpers
+{
+	[HtmlTargetElement("div", Attributes = "title")]
+    public class ContentWrapperTagHelper : TagHelper
+    {
+		public bool IncludeHeader { get; set; } = true;
+		public bool IncludeFooter { get; set; } = true;
+
+		public string Title { get; set; }
+
+	    public override void Process(TagHelperContext context, TagHelperOutput output)
+	    {
+		    output.Attributes.SetAttribute("class", "panel-body");
+
+			var title = new TagBuilder("h1");
+		    title.InnerHtml.Append(Title);
+
+			var container = new TagBuilder("div");
+		    container.Attributes["class"] = "bg-info panel-body";
+		    container.InnerHtml.AppendHtml(title);
+
+
+			if (IncludeHeader)
+		    {
+			    output.PreElement.SetHtmlContent(container);
+		    }
+
+		    if (IncludeFooter)
+		    {
+				output.PostElement.SetHtmlContent(container);
+			}
+	    }
+    }
+}
